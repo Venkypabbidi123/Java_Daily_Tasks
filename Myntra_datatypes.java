cb0cf508611696cbb3eccbacb230cc0b72177c41
Venkypabbidi123 <venkypabbidi72738@gmail.com>
2025-06-06 18:08:51 +0530

    Datatypes are created

---
+++ Myntra_datatypes.java
@@ -0,0 +1,23 @@
+public class Myntra_datatypes {
+
+  public static void main(String[] args) {
+    String brand = "HIGHLANDER";
+    String description = "Men Comfort Relaxed Fit Textured Spread Collar Casual Shirt";
+    float rating = 4.6f;
+    byte outoff = 5;
+    int price = 750;
+    float discount = 81.0f;
+    int max_retail_price = 3949;
+    int selling_price = 750;
+    byte size = 40;
+    System.out.println(brand);
+    System.out.println(description);
+    System.out.print(rating);
+    System.out.println(" " + outoff + " " + "Ratings");
+    System.out.print("₹" + "" + price);
+    System.out.println("   " + discount + "%" + "  OFF");
+    System.out.println(max_retail_price);
+    System.out.println(selling_price);
+    System.out.println(size);
+  }
+}
