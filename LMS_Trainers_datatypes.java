bbf07294e30e8afdd77e8a7d9bd73174de21779f
Venkypabbidi123 <venkypabbidi72738@gmail.com>
2025-06-09 21:52:03 +0530

    LMS_Trainers_Datatype

---
+++ LMS_Trainers_datatypes.java
@@ -0,0 +1,23 @@
+public class LMS_Trainers_datatypes {
+  public static void main(String[] args) {
+    byte trainer_level = 5;
+    short number_of_courses = 120;
+    int trainer_ID = 1001;
+    long total_students_taught = 500000;
+    float trainer_rating = 4.6f;
+    double salary = 95000.75;
+    char grade = 'A';
+    boolean is_Active = true;
+    String trainer_name = "Ravi Kumar";
+
+    System.out.println("Trainer level : " + trainer_level);
+    System.out.println("Number of Courses : " + number_of_courses);
+    System.out.println("Trainer_ID : " + trainer_ID);
+    System.out.println("Total Students Taught : " + total_students_taught);
+    System.out.println("Trainer rating : " + trainer_rating);
+    System.out.println("Salary : " + salary);
+    System.out.println("Grade : " + grade);
+    System.out.println("IS_Active : " + is_Active);
+    System.out.println("Trainer_Name : " + trainer_name);
+  }
+}
