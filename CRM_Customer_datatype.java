1e6df0bd19ffd8d0db938210e2a8e427fac46112
Venkypabbidi123 <venkypabbidi72738@gmail.com>
2025-06-10 18:24:37 +0530

    CRM_Customer_datatype

---
+++ CRM_Customer_datatype.java
@@ -0,0 +1,25 @@
+public class CRM_Customer_datatype {
+  public static void main(String[] args) {
+    short customer_ID = 101;
+    String name = "Ganesh";
+    String email = "Ganesh@gmail.com";
+    long contact_number = 9876543210l;
+    char gender = 'M';
+    byte age = 28;
+    float rating = 4.5f;
+    double credit_limit = 250000.34;
+    boolean is_Active = true;
+    String created_date = "2024-06-10";
+    
+    System.out.println("Customer_Id : " + customer_ID);
+    System.out.println("Name : " + name);
+    System.out.println("Email_ID : " + email);
+    System.out.println("Contact_number : " + contact_number);
+    System.out.println("Gender : " + gender);
+    System.out.println("AGE : " + age);
+    System.out.println("Rating : " + rating);
+    System.out.println("Credit_Limit" + credit_limit);
+    System.out.println("IS_ACTIVE : " + is_Active);
+    System.out.println("Created_Date : " + created_date);
+  }
+}
