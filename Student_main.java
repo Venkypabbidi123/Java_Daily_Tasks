cbbca96846f9c4f9cd1c3a7b51dec12d21f4ead7
Venkypabbidi123 <venkypabbidi72738@gmail.com>
2025-06-20 11:23:04 +0530

    Student_main_class

---
+++ Student_main.java
@@ -0,0 +1,11 @@
+public class Student_main {
+  public static void main(String[] args) {
+     Student stud = new  Student();
+     stud.name = "venky";
+     stud.moblienumber = 9080549508l;
+     stud.age = 23;
+     stud.course = "Java";
+     stud.details();
+  }
+  
+}
