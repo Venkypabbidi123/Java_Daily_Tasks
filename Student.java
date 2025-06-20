22912db727482d4485a181075747113ceaf3069e
Venkypabbidi123 <venkypabbidi72738@gmail.com>
2025-06-20 11:22:29 +0530

    Student class

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+  String name;
+  long moblienumber;
+  byte age;
+  String course;
+   public void details(){
+    System.out.println("Name of the student : " + name);
+    System.out.println("Student Mobile Number  : " + moblienumber);
+    System.out.println("student age : " + age);
+    System.out.println("Course : " + course);
+   }
+}
